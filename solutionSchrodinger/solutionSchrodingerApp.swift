507b559326078a32a6644552fe11f26234d05249
Matt Ventrelli <mventrelli@hawk.iit.edu>
2023-02-16 14:44:34 -0600

    Initial Commit

---
+++ solutionSchrodinger/solutionSchrodingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  solutionSchrodingerApp.swift
+//  solutionSchrodinger
+//
+//  Created by IIT PHYS 440 on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct solutionSchrodingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
